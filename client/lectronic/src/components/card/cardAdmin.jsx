29f521aa20fd88fb7ffc614fa4d567c342567410
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-10-20 00:13:52 +0700

    Slicing card component

--- client/lectronic/src/components/card/cardAdmin.jsx
+++ client/lectronic/src/components/card/cardAdmin.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import img from "../../img/product-example.png";
+import { BsTrash } from "react-icons/bs";
+import "./style.css";
 
 function CardAdmin() {
   return (
-    <div>
-      <h1>CardAdmin</h1>
+    <div className="card-admin">
+      <div className="row">
+        <div className="col-lg-6">
+          <div className="card-admin-image-bg mr-2">
+            <img className="card-admin-image" src={img} alt="image" />
+          </div>
+          <div className="left-detail">
+            <span className="badge card-admin-type">Headphone</span>
+            <div className="row">
+              <div className="card-admin-title">Sennheiser HD-25</div>
+              <div className="card-admin-price">$3000</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 mt-auto">
+          <div className="row">
+            <button type="button" class="card-admin-button-edit">
+              Detail
+            </button>
+            <button type="button" class="card-admin-button-trash">
+              <BsTrash className="bs-cart" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- client/lectronic/src/components/card/cardOriginal.jsx
+++ client/lectronic/src/components/card/cardOriginal.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import img from "../../img/product-example.png";
+import { BsCart } from "react-icons/bs";
+import "./style.css";
 
 function CardOriginal() {
   return (
-    <div>
-      <h1>CardOriginal</h1>
+    <div className="card-original">
+      <div className="card-title">Sennheiser HD-25</div>
+      <div className="row">
+        <div className="col-6 card-price">$3000</div>
+        <div className="col-6 card-rating">4.9</div>
+      </div>
+      <img className="card-image" src={img} alt="image" />
+      <div className="row">
+        <button type="button" class="card-button-detail">
+          Detail
+        </button>
+        <button type="button" class="card-button-cart">
+          <BsCart className="bs-cart" />
+        </button>
+      </div>
     </div>
   );
 }
