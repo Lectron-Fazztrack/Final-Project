36758c696925c8fe2be7bc1b162806c2a45d857a
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-10-20 02:24:57 +0700

    Add logo for favicon

--- client/lectronic/src/pages/home/index.jsx
+++ client/lectronic/src/pages/home/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import CardAdmin from "../../components/card/cardAdmin";
+import CardHistory from "../../components/card/cardHistory";
+import CardOriginal from "../../components/card/cardOriginal";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-4 col-lg-6 col-sm-12">
+            <CardOriginal />
+          </div>
+          <div className="col-xl-4 col-lg-6 col-sm-12">
+            <CardOriginal />
+          </div>
+          <div className="col-xl-4 col-lg-6 col-sm-12">
+            <CardOriginal />
+          </div>
+        </div>
+        <div className="row">
+          <CardAdmin />
+        </div>
+        <div className="row">
+          <CardHistory />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
