2565b11208301df24752527facda136e26241659
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-10-20 17:38:43 +0700

    Update home

--- client/lectronic/src/pages/home/index.jsx
+++ client/lectronic/src/pages/home/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import CardOriginal from "../../components/card/cardOriginal";
 import subscribe from "../../img/subscribe.png";
-import { BsSearch } from "react-icons/bs";
+import { BsSearch, BsBag, BsCashCoin, BsBox } from "react-icons/bs";
 import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
 import "./style.css";
 
 function Home() {
@@ -10,8 +11,62 @@ function Home() {
     <div className="App">
       <Header />
       <div className="container">
+        <div className="row row-badge">
+          <button type="button" class="home-badge">
+            How it Works
+          </button>
+        </div>
+        <div className="row home-title text-center">
+          Make An
+          <br />
+          Order Easily
+        </div>
         <div className="row">
-          <span className="home-badge text-center">Our Product</span>
+          <div className="col-xl-4 col-lg-6">
+            <div className="home-card">
+              <button className="home-card-icon">
+                <BsBag className="home-bs-icon" />
+              </button>
+              <div className="home-card-title">Select Product</div>
+              <div className="home-card-desc">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-6">
+            <div className="home-card">
+              <button className="home-card-icon">
+                <BsCashCoin className="home-bs-icon" />
+              </button>
+              <div className="home-card-title">Make Payment</div>
+              <div className="home-card-desc">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-6">
+            <div className="home-card">
+              <button className="home-card-icon">
+                <BsBox className="home-bs-icon" />
+              </button>
+              <div className="home-card-title">Receive Product</div>
+              <div className="home-card-desc">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <button type="button" class="home-badge">
+            Our Product
+          </button>
         </div>
 
         <div className="row home-title text-center">
@@ -82,9 +137,6 @@ function Home() {
           </div>
           <div className="col-lg-6">
             <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">
-                <BsSearch />
-              </span>
               <input
                 type="text"
                 class="form-control"
@@ -92,6 +144,9 @@ function Home() {
                 aria-label="Username"
                 aria-describedby="basic-addon1"
               ></input>
+              <span class="input-group-text" id="basic-addon1">
+                <BsSearch />
+              </span>
             </div>
           </div>
         </div>
@@ -186,6 +241,7 @@ function Home() {
           <img src={subscribe} alt="" />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
