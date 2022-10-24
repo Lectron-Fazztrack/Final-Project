3bb6e95557081249bb3558847553f2a05aaecd0a
Adiet Alimudin <55053994+adiet95@users.noreply.github.com>
2022-10-24 08:32:54 +0700

    update user

--- server/src/interfaces/user.go
+++ server/src/interfaces/user.go
@@ -6,19 +6,10 @@ import (
 )
 
 type UserRepo interface {
-	FindAll(limit, offset int) (*models.Users, error)
-	Save(data *models.User) (*models.User, error)
 	UpdateUser(data *models.User, email string) (*models.User, error)
-	DeleteUser(email string) (*models.User, error)
 	FindByEmail(email string) (*models.User, error)
-	FindByName(name string) (*models.Users, error)
 }
 
 type UserService interface {
-	Add(data *models.User) *libs.Response
 	Update(data *models.User, email string) *libs.Response
-	Delete(email string) *libs.Response
-	FindEmail(email string, limit, offset int) *libs.Response
-	Search(email string) *libs.Response
-	SearchName(name string) *libs.Response
 }
--- server/src/modules/users/user_service.go
+++ server/src/modules/users/user_service.go
@@ -14,25 +14,6 @@ func NewService(reps interfaces.UserRepo) *user_service {
 	return &user_service{reps}
 }
 
-func (re *user_service) Add(data *models.User) *libs.Response {
-	valid := libs.Validation(data.Email, data.Password)
-	if valid != nil {
-		return libs.New(valid.Error(), 400, true)
-	}
-
-	hassPass, err := libs.HashPassword(data.Password)
-	if err != nil {
-		return libs.New(err.Error(), 400, true)
-	}
-
-	data.Password = hassPass
-	result, err := re.user_repo.Save(data)
-	if err != nil {
-		return libs.New(err.Error(), 400, true)
-	}
-	return libs.New(result, 201, false)
-}
-
 func (re *user_service) Update(data *models.User, email string) *libs.Response {
 	//Get old
 	oldData, err := re.user_repo.FindByEmail(email)
@@ -60,44 +41,4 @@ func (re *user_service) Update(data *models.User, email string) *libs.Response {
 		return libs.New(err.Error(), 400, true)
 	}
 	return libs.New(result, 202, false)
-}
-
-func (re *user_service) Delete(email string) *libs.Response {
-
-	data, err := re.user_repo.DeleteUser(email)
-	if err != nil {
-		return libs.New(err.Error(), 400, true)
-	}
-	return libs.New(data, 204, false)
-}
-
-func (re *user_service) FindEmail(email string, limit, offset int) *libs.Response {
-	data, err := re.user_repo.FindByEmail(email)
-	if err != nil {
-		return libs.New(err.Error(), 400, true)
-	}
-	if data.Role == "admin" {
-		datas, err := re.user_repo.FindAll(limit, offset)
-		if err != nil {
-			return libs.New(err.Error(), 400, true)
-		}
-		return libs.New(datas, 200, false)
-	}
-	return libs.New(data, 200, false)
-}
-
-func (re *user_service) Search(email string) *libs.Response {
-	data, err := re.user_repo.FindByEmail(email)
-	if err != nil {
-		return libs.New(err.Error(), 400, true)
-	}
-	return libs.New(data, 200, false)
-}
-
-func (re *user_service) SearchName(name string) *libs.Response {
-	data, err := re.user_repo.FindByName(name)
-	if err != nil {
-		return libs.New(err.Error(), 400, true)
-	}
-	return libs.New(data, 200, false)
 }
