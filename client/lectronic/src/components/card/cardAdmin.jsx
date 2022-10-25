83c2a1e54583237b7c2c029e084f486a0069d3a6
Muhammad Chirzul Maula <chirzul@protonmail.com>
2022-10-25 07:16:36 +0700

    delete confirmation

--- client/lectronic/src/components/card/cardAdmin.jsx
+++ client/lectronic/src/components/card/cardAdmin.jsx
@@ -1,18 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsTrash } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import useApi from "../../helpers/api";
 import "./style.css";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 function CardAdmin(props) {
   const navigate = useNavigate();
-  const detail = () => {
-    navigate("/products/detail/" + props.id);
-  };
 
   const api = useApi();
 
-  const deleteProduct = (id) => {
+  const deleteProduct = () => {
     api
       .req({
         method: "DELETE",
@@ -25,6 +24,72 @@ function CardAdmin(props) {
         console.log(err);
       });
   };
+
+  const [data, setData] = useState({});
+  const [product, setProduct] = useState({});
+  const { token } = useSelector((state) => state.users);
+
+  const onChangeInput = (event) => {
+    event.preventDefault();
+
+    const tmpdata = { ...data };
+    tmpdata[event.target.name] = event.target.value;
+    setData(tmpdata);
+  };
+
+  const onChangeFile = (event) => {
+    event.preventDefault();
+
+    const file = event.target.files[0];
+    if (file) {
+      const tmpdata = { ...data };
+      tmpdata["image"] = file;
+      setData(tmpdata);
+    }
+  };
+
+  const getProducts = () => {
+    api
+      .req({
+        method: "GET",
+        url: `/products/` + props.id,
+      })
+      .then((res) => {
+        const { data } = res.data;
+        setProduct(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const postData = async () => {
+    const formData = new FormData();
+    for (const key in data) {
+      formData.append(`${key}`, data[key]);
+    }
+
+    axios({
+      method: "POST",
+      url: "http://localhost:8080/products",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+      data: formData,
+    })
+      .then((res) => {
+        navigate("/products");
+      })
+      .catch((err) => {
+        console.log(data);
+        alert(err);
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
     <div className="card-admin">
@@ -44,19 +109,128 @@ function CardAdmin(props) {
         <div className="col-lg-6 mt-auto">
           <div className="row">
             <button
-              onClick={detail}
               type="button"
               class="card-admin-button-edit"
+              data-bs-toggle="modal"
+              data-bs-target="#editModal"
             >
               Detail
             </button>
             <button
-              onClick={deleteProduct}
+              onClick={() => {
+                if (window.confirm("Are you sure to delete this vehicle?")) {
+                  deleteProduct();
+                }
+              }}
               type="button"
               class="card-admin-button-trash"
             >
               <BsTrash className="bs-cart" />
             </button>
+          </div>
+        </div>
+      </div>
+      <div
+        class="modal fade"
+        id="editModal"
+        tabindex="-1"
+        aria-labelledby="editModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content modal-contents">
+            <div class="modal-header">
+              <h1 class="modal-title" id="editModalLabel">
+                Edit Product
+              </h1>
+            </div>
+            <div class="modal-body">
+              <div className="row">
+                <div className="col-lg-6">
+                  <label class="form-label form-labels">Name</label>
+                  <input
+                    name="name"
+                    type="text"
+                    defaultValue={data.name}
+                    class="form-control form-control-lg mb-3 modal-forms"
+                    placeholder="Input name product..."
+                    onChange={onChangeInput}
+                  ></input>
+                </div>
+                <div className="col-lg-6">
+                  <label class="form-label form-labels">Price</label>
+                  <input
+                    name="price"
+                    type="number"
+                    defaultValue={data.price}
+                    class="form-control form-control-lg mb-3 modal-forms"
+                    placeholder="Input price product..."
+                    onChange={onChangeInput}
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-6">
+                  <label class="form-label form-labels">Stock</label>
+                  <input
+                    name="stock"
+                    type="number"
+                    defaultValue={data.stock}
+                    class="form-control form-control-lg mb-3 modal-forms"
+                    placeholder="Input stock product..."
+                    onChange={onChangeInput}
+                  ></input>
+                </div>
+                <div className="col-lg-6">
+                  <label class="form-label form-labels">Description</label>
+                  <input
+                    name="description"
+                    type="text"
+                    defaultValue={data.description}
+                    class="form-control form-control-lg mb-3 modal-forms"
+                    placeholder="Input description product..."
+                    onChange={onChangeInput}
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-6">
+                  <label class="form-label form-labels">Category</label>
+                  <select
+                    name="type"
+                    className="form-select modal-forms"
+                    defaultValue={data.category}
+                    onChange={onChangeInput}
+                  >
+                    <option value="" selected disabled hidden>
+                      {data.category}
+                    </option>
+                    <option value="Headphone">Headphone</option>
+                    <option value="Air Conditioner">Air Conditioner</option>
+                    <option value="Television">Television</option>
+                    <option value="Router">Router</option>
+                  </select>
+                </div>
+                <div className="col-lg-6">
+                  <label class="form-label form-labels">Image (max 5)</label>
+                  <input
+                    name="image"
+                    type="file"
+                    class="form-control mb-3 modal-forms"
+                    onChange={onChangeFile}
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="modal-button-submit mt-3"
+                onClick={postData}
+              >
+                Submit
+              </button>
+            </div>
           </div>
         </div>
       </div>
